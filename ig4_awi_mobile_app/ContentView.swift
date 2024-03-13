8a4cdf20a1c016da260f00306dfe0ee070169155
TAIP25 <bouleon25@gmail.com>
2024-03-13 09:33:58 +0100

    Initial Commit

---
+++ ig4_awi_mobile_app/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ig4_awi_mobile_app
+//
+//  Created by Léon Boudier on 13/03/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
